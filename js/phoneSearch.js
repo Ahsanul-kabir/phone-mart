e2bb8863f4c2ef5e8039f20cc28ca55a08900e86
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-03-02 20:14:57 +0600

    single details added

--- js/phoneSearch.js
+++ js/phoneSearch.js
@@ -31,11 +31,11 @@ const displaySearchPhones = (phones) => {
             const div = document.createElement('div')
             div.classList.add('col');
             div.innerHTML = `
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        <img src="${phone.image}" class="card-img-top w-75" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">Brand: ${phone.brand}</p>
-            <a class="btn btn-primary">Details</a>
+            <a class="btn btn-primary text-center" onclick="loadSinglePhone('${phone.slug}')">Details</a>
         </div>
         `
             displayPhone.appendChild(div)
@@ -43,3 +43,37 @@ const displaySearchPhones = (phones) => {
     }
 }
 
+const loadSinglePhone = async (phoneId) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+
+    const res = await fetch(url)
+    const data = await res.json()
+    displayPhoneDetails(data.data)
+}
+
+const displayPhoneDetails = (phone) => {
+    const singlePhoneDetail = document.getElementById('single-phone-detail')
+    singlePhoneDetail.innerHTML = ''
+    singlePhoneDetail.style.display = "block"
+    const div = document.createElement('div')
+    div.className = 'd-flex justify-content-center'
+
+    div.innerHTML = `
+    <div class="col-lg-4 col-md-6 col-sm-12">
+        <img src="${phone.image}" alt="" class="img-fluid w-75">
+        <p>Released ${phone.releaseDate || 'No Release Date found!'}</p>
+    </div>
+    <div class="col-lg-8 col-md-6 col-sm-12">
+        <h4>${phone.name}</h4>
+        <h6>${phone.brand}</h6>
+
+        <h2>Features</h2>
+        <p>Storage: ${phone.mainFeatures.storage}</p>
+        <p>DisplaySize: ${phone.mainFeatures.displaySize}</p>
+        <p>ChipSet: ${phone.mainFeatures.chipSet}</p>
+        <p>Memory: ${phone.mainFeatures.memory}</p>
+        <p>Sensors: ${phone.mainFeatures.sensors}</p>
+    </div>
+    `
+    singlePhoneDetail.appendChild(div)
+}
